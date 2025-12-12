ff0b87303bc80a017d3998771087bc3a272265d6
Elif Polat <elif7polat@gmail.com>
2025-12-12 17:56:30 +0300

    createHandler classes added

---
+++ Core/MovieApi.Application/Features/Handlers/CategoryHandlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using MovieApi.Application.Features.Commands.CategoryCommands;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.Handlers.CategoryHandlers;
+
+public class CreateCategoryCommandHandler
+{
+    private readonly MovieContext _context;
+
+    public CreateCategoryCommandHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async void Handle(CreateCategoryCommand command)
+    {
+        _context.Categories.Add(new Category
+        {
+            Name = command.Name,
+        });
+
+        await _context.SaveChangesAsync();
+    }
+}
