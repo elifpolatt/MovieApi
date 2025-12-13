093bd54e75ca8ec05e4cc972765a262fa8b04f4c
Elif Polat <elif7polat@gmail.com>
2025-12-13 21:32:54 +0300

    handler classes

---
+++ Core/MovieApi.Application/Features/Handlers/CategoryHandlers/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.Commands.CategoryCommands;
+using Persistence.Context;
+
+namespace MovieApi.Application.Features.Handlers.CategoryHandlers;
+public class UpdateCategoryCommandHandler
+{
+    private readonly MovieContext _context;
+
+    public UpdateCategoryCommandHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async void Handle(UpdateCategoryCommand command)
+    {
+        var categories = await _context.Categories.FindAsync(command.Id);
+        categories.Name = command.Name;
+
+        await _context.SaveChangesAsync();
+    }
+}
+
---
+++ Core/MovieApi.Application/Features/Handlers/MovieHandlers/DeleteMovieCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.Commands.MovieCommands;
+using Persistence.Context;
+
+namespace MovieApi.Application.Features.Handlers.MovieHandlers;
+
+public class DeleteMovieCommandHandler
+{
+    private readonly MovieContext _context;
+
+    public DeleteMovieCommandHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async void Handle(DeleteMovieCommand command)
+    {
+        var movies = await _context.Movies.FindAsync(command.Id);
+        _context.Movies.Remove(movies);
+
+        await _context.SaveChangesAsync();
+    }
+}
+
