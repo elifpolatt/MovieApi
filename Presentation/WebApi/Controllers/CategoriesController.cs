b6b17d7acb952884149aa0442359a873a85f7e60
Elif Polat <elif7polat@gmail.com>
2025-12-17 23:18:16 +0300

    categoriesController oluşturuldu

--- Presentation/WebApi/Controllers/CategoriesController.cs
+++ Presentation/WebApi/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.Commands.CategoryCommands;
 using MovieApi.Application.Features.Handlers.CategoryHandlers;
 using MovieApi.Application.Features.Queries.CategoryQueries;
 
@@ -9,5 +10,34 @@ namespace WebApi.Controllers
     public class CategoriesController : Controller
     {
         private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly GetCategoryByIdQuery _getCategoryById;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly DeleteCategoryCommandHandler _deleteCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQuery getCategoryById, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler,
+            DeleteCategoryCommandHandler deleteCategoryCommandHandler
+            )
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _getCategoryById = getCategoryById;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _deleteCategoryCommandHandler = deleteCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var value = await _getCategoryQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok();
+        }
     }
 }
