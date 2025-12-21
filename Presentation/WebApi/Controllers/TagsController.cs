939263cdb8e16d7003d419ea8d7fbfdbbc006f1c
Elif Polat <elif7polat@gmail.com>
2025-12-21 23:45:12 +0300

    tag controller added

---
+++ Presentation/WebApi/Controllers/TagsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TagsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TagsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
+
